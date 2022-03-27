8bce0e1b26a2458eb382ee103049c4566e588729
Syahir Amali <syahiramali@gmail.com>
2022-03-27 19:17:25 +0800

    configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require('dotenv').config()
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -17,5 +17,41 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      gas: 12000000,
+      allowUnlimitedContractSize: true,
+      blockGasLimit: 0x1fffffffffffff,
+      mining: {
+        auto: true,
+        interval: 5000
+      },
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+        settings: {},
+      },
+      {
+        version: "0.8.4",
+        settings: {},
+      },
+      {
+        version: "0.8.11",
+        settings: {},
+      },
+      {
+        version: "0.8.13",
+        settings: {},
+      },
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      }
+    },
+  },
 };
